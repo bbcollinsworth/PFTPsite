5def4881131cb8107e2572a4822015e5ebeab688
jojoychin <jochin@Joannas-MacBook-Air.local>
2016-03-15 11:13:15 -0400

    changed css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-$('#signup-container').one('submit',function(){
+(function() {
+
+var makeForm = function(tag, pic) {
+  $('#signup-container').one('submit',function(){
         var inputEmail = encodeURIComponent($('#signup-field').val());
-        var baseURL = 'https://docs.google.com/forms/d/1_VKPGtjca8RbKaJ_RZG5Ixs_ry7_REER_tKB7poYrw8/formResponse?entry.924917837=';
+        var baseURL = 'https://docs.google.com/forms/d/1_VKPGtjca8RbKaJ_RZG5Ixs_ry7_REER_tKB7poYrw8/formResponse?';
+        var emailEntry = 'entry.924917837'
+        var tagEntry = 'entry.646248454';
+        var picEntry = 'entry.1114145375';
         var submitRef = '&submit=Submit';
-        var submitURL = (baseURL + inputEmail + submitRef);
+        var submitURL = (baseURL + emailEntry + "=" + inputEmail + "&" + tagEntry + "=" + tag + "&" + picEntry + "=" + pic + submitRef);
+        // var submitURL = (baseURL + inputEmail + submitRef);
         console.log(submitURL);
         $(this)[0].action=submitURL;
         $('#signup-field').addClass('active').val('');
         alert('Thank you for signing up to be a beta tester! We\'ll be in touch shortly.');
     });
-
-(function() {
+};
 
    var taglines = [
       "History lives on your doorstep.",
@@ -38,6 +44,8 @@ $('#signup-container').one('submit',function(){
 
       console.log("Tagline set to: " + pickedTag);
       console.log("BG set to: " + pickedPic);
+
+      makeForm(pickedTag, pickedPic);
    };
 
    changeItUp();
